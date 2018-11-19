c7d6f0ddf69c298aa020eba192e9b52eea8f3105
rajjayabalan <44099116+rajjayabalan@users.noreply.github.com>
2018-11-19 10:14:02 +0530

    Add files via upload

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.sass']
+})
+export class HomeComponent implements OnInit {
+
+  h1Style: boolean = false;
+  users: Object;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.getUsers().subscribe(data => {
+        this.users = data
+        console.log(this.users);
+      }
+    );
+  }
+
+  firstClick() {
+    this.data.firstClick();
+  }
+
+}
